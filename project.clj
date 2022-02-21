c2e3f079b2ac76a4acc3ecd0029679ce890914f5
mcorbin <corbin.math@gmail.com>
2022-02-21 22:15:21 +0100

    Bump dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "3.12.3"]
                  [com.boundary/high-scale-lib "1.0.6"]
-                 [com.google.protobuf/protobuf-java "3.19.1"]
-                 [com.google.protobuf/protobuf-java-util "3.19.1"]
-                 [com.influxdb/influxdb-client-java "3.4.0"]
+                 [com.google.protobuf/protobuf-java "3.19.4"]
+                 [com.google.protobuf/protobuf-java-util "3.19.4"]
+                 [com.influxdb/influxdb-client-java "4.3.0"]
                  [http-kit "2.5.3"]
                  [io.netty/netty-all "4.1.58.Final"]
                  [io.netty/netty-codec "4.1.58.Final"]
@@ -16,9 +16,9 @@
                  [io.netty/netty-common "4.1.58.Final"]
                  [io.netty/netty-transport "4.1.58.Final"]
                  [io.netty/netty-resolver "4.1.58.Final"]
-                 [fr.mcorbin/corbihttp "0.27.0"]
+                 [fr.mcorbin/corbihttp "0.28.0"]
                  [org.clojure/clojure "1.10.3"]
-                 [org.clojure/spec.alpha "0.2.194"]
+                 [org.clojure/spec.alpha "0.3.218"]
                  [org.elasticsearch.client/elasticsearch-rest-client "7.14.1"]
                  [org.xerial.snappy/snappy-java "1.1.8.4"]
                  [riemann-clojure-client "0.5.3"]]
@@ -32,7 +32,7 @@
           :output-path "site/mirabelle/static/generated-doc"
           :metadata   {:doc/format :markdown}}
   :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.11.0"]
-                                  [org.clojure/tools.namespace "1.1.0"]
+                                  [org.clojure/tools.namespace "1.2.0"]
                                   [org.clojure/data.fressian "1.0.0"]
                                   [com.clojure-goes-fast/clj-memory-meter "0.1.3"]]
                    :plugins [[lein-ancient "0.6.15"]
