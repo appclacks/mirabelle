6c7c209a277a14662116a82feb5768472c2aab0a
mcorbin <corbin.math@gmail.com>
2024-04-18 22:41:45 +0200

    remove prometheus output wip code

--- src/clojure/mirabelle/prometheus.clj
+++ src/clojure/mirabelle/prometheus.clj
@@ -34,33 +34,33 @@
       events)))
 
 
-(defn events->write-request
-  "Converts list of events to a Prometheus remote write request"
-  [events labels]
-  (let [write-request (Remote$WriteRequest/newBuilder)
-        timeseries (atom {})]
-    (doseq [event events]
-      (let [labels (-> (select-keys event labels)
-                       (assoc "__name__" (:service event)))
-            ^prometheus.Types$TimeSeries$Builder timeserie
-            (get
-             @timeseries
-             labels
-             (let [timeserie (Types$TimeSeries/newBuilder)]
-               (doseq [[k v] labels]
-                 (.addLabels timeserie
-                             (doto (Types$Label/newBuilder)
-                               (.setName (name k))
-                               (.setValue (name v))
-                               (.build))))
-               (swap! timeseries assoc labels timeserie)
-               timeserie))]
-        (println @timeseries)
-        (.addSamples timeserie
-                     (doto (Types$Sample/newBuilder)
-                       (.setValue (:metric event 0))
-                       (.setTimestamp (int (* 1000 (:time event))))
-                       (.build)))))
-    (doseq [[_ ^prometheus.Types$TimeSeries$Builder timeserie] @timeseries]
-      (.addTimeseries write-request (.build timeserie)))
-    (.build write-request)))
+;; (defn events->write-request
+;;   "Converts list of events to a Prometheus remote write request"
+;;   [events labels]
+;;   (let [write-request (Remote$WriteRequest/newBuilder)
+;;         timeseries (atom {})]
+;;     (doseq [event events]
+;;       (let [labels (-> (select-keys event labels)
+;;                        (assoc "__name__" (:service event)))
+;;             ^prometheus.Types$TimeSeries$Builder timeserie
+;;             (get
+;;              @timeseries
+;;              labels
+;;              (let [timeserie (Types$TimeSeries/newBuilder)]
+;;                (doseq [[k v] labels]
+;;                  (.addLabels timeserie
+;;                              (doto (Types$Label/newBuilder)
+;;                                (.setName (name k))
+;;                                (.setValue (name v))
+;;                                (.build))))
+;;                (swap! timeseries assoc labels timeserie)
+;;                timeserie))]
+;;         (println @timeseries)
+;;         (.addSamples timeserie
+;;                      (doto (Types$Sample/newBuilder)
+;;                        (.setValue (:metric event 0))
+;;                        (.setTimestamp (int (* 1000 (:time event))))
+;;                        (.build)))))
+;;     (doseq [[_ ^prometheus.Types$TimeSeries$Builder timeserie] @timeseries]
+;;       (.addTimeseries write-request (.build timeserie)))
+;;     (.build write-request)))
