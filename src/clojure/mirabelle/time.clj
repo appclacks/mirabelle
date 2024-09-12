77fe1b9bdffdab51719addb60bdb5a24129a0ef6
mcorbin <corbin.math@gmail.com>
2024-09-13 00:28:13 +0200

    Time: fix formatting

--- src/clojure/mirabelle/time.clj
+++ src/clojure/mirabelle/time.clj
@@ -9,22 +9,18 @@
 
 (defn s->ns
   [seconds]
-  (* seconds 1000000000)
-  )
+  (* seconds 1000000000))
 
 
 (defn ms->ns
   [milliseconds]
-  (* milliseconds 1000000)
-  )
+  (* milliseconds 1000000))
 
 (defn us->ns
   [microseconds]
-  (* microseconds 1000)
-  )
+  (* microseconds 1000))
 
 
 (defn ns->ms
   [nanoseconds]
-  (long (/ nanoseconds 1000000))
-  )
+  (long (/ nanoseconds 1000000)))
